41edf0227b466789439ef4c49e37b0a7b1df923f
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2023-02-28 17:21:02 +0530

    accounts folder from views imported

--- configs/theme.config.js
+++ configs/theme.config.js
@@ -1,21 +1,51 @@
-import { THEME_ENUM } from 'constants/theme.constant'
+import { THEME_ENUM } from "constants/theme.constant";
 
 /**
  * Since some configurations need to be match with specific themes,
  * we recommend to use the configuration that generated from demo.
  */
 
+// export const themeConfig = {
+//     themeColor: 'indigo',
+//     direction: THEME_ENUM.DIR_LTR,
+//     mode: THEME_ENUM.MODE_LIGHT,
+//     primaryColorLevel: 600,
+//     cardBordered: true,
+//     panelExpand: false,
+//     controlSize: 'md',
+//     navMode: THEME_ENUM.NAV_MODE_LIGHT,
+//     layout: {
+//         type: THEME_ENUM.LAYOUT_TYPE_MODERN,
+//         sideNavCollapse: false,
+//     },
+// }
+
+// export const themeConfig = {
+//   themeColor: "indigo",
+//   direction: "ltr",
+//   mode: "dark",
+//   primaryColorLevel: 600,
+//   cardBordered: true,
+//   panelExpand: false,
+//   controlSize: "md",
+//   navMode: "transparent",
+//   layout: {
+//     type: "modern",
+//     sideNavCollapse: false,
+//   },
+// };
+
 export const themeConfig = {
-    themeColor: 'indigo',
-    direction: THEME_ENUM.DIR_LTR,
-    mode: THEME_ENUM.MODE_LIGHT,
-    primaryColorLevel: 600,
-    cardBordered: true,
-    panelExpand: false,
-    controlSize: 'md',
-    navMode: THEME_ENUM.NAV_MODE_LIGHT,
-    layout: {
-        type: THEME_ENUM.LAYOUT_TYPE_MODERN,
-        sideNavCollapse: false,
-    },
-}
+  themeColor: "indigo",
+  direction: "ltr",
+  mode: "dark",
+  primaryColorLevel: 600,
+  cardBordered: false,
+  panelExpand: false,
+  controlSize: "md",
+  navMode: "dark",
+  layout: {
+    type: "decked",
+    sideNavCollapse: false,
+  },
+};
